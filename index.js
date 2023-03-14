8061a5273c1cac125e237ab0ece0efbd9908aa8d
LautaroNav <78655049+LautaroNav@users.noreply.github.com>
2023-03-14 00:27:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 console.log( "Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo.")
+
+
+      let name = "Lautaro";
+      let yearsOld = 22 ;
+      const developer = true
+
+      const dateBearthday = new Date(1997, 11, 16)
+      console.log("Mi cumpleaños es ", dateBearthday)
+
+ 
+ 
+ 
+      const favoriteBook = {
+            tittle: 'Hunter X Hunter ',
+            author: 'Togashi',
+            fecha: new Date(1998, 2, 3),
+            url: "https://es.wikipedia.org/wiki/Hunter_%C3%97_Hunter"
+      }
+
+      console.log(favoriteBook)
